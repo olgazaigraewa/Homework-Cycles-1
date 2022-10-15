2f0d98e540c0ed48f14ce1a3690dc5386c421eda
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-10-15 12:44:10 +0300

    Homework -Cycles-1 (повторно)

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         for (int i = 2; i < 17; i += 2) {
             System.out.println(+i);
         }
-         //Задание 4
+        //Задание 4
         System.out.println("Задание 4");
         for (int i = 10; i >= -10; i--) {
             System.out.println(+i);
@@ -25,12 +25,12 @@ public class Main {
 
         //Домашнее задание-2
         System.out.println("     Домашнее задание-2");
-         //Задание 1
+        //Задание 1
         System.out.println("Задание 1");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
-         //Задание 2
+        //Задание 2
         System.out.println("Задание 2");
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(+i);
@@ -42,11 +42,11 @@ public class Main {
         }
         //Домашнее задание-3
         System.out.println("     Домашнее задание-3");
-         //Задание 1
+        //Задание 1
         System.out.println("Задание 1");
         int cash = 29_000;
         int total = 0;
-        for(int i = 1; i <= 12; i++) {
+        for (int i = 1; i <= 12; i++) {
             total += cash;
             System.out.println(" Месяц " + i + ", сумма накоплений равна " + total + " рублей. ");
         }
@@ -54,10 +54,21 @@ public class Main {
         System.out.println("Задание 2");
         double totalWithPercent = 0;
         for (int i = 1; i <= 12; i++) {
-            totalWithPercent = (totalWithPercent+cash)* 1.01 ;
-            System.out.println(" Месяц " + i + ", сумма накоплений равна " + totalWithPercent +  " рублей. ");
+            totalWithPercent = (totalWithPercent + cash) * 1.01;
+            System.out.println(" Месяц " + i + ", сумма накоплений равна " + totalWithPercent + " рублей. ");
         }
+
     }
 
-}
+        }
+
+
+
+
+
+
+
+
+
+
 
